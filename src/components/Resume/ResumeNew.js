5505cdf6b805f9ca25fd4c63e15521b9dffc1858
javierjericm <javierjericm@gmail.com>
2025-05-02 13:01:18 +0800

    Updated resume section

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -31,22 +31,16 @@ function ResumeNew() {
           </Button>
         </Row>
 
-        <Row className="resume">
+        <Row className="resume1">
           <Document file={pdf} className="d-flex justify-content-center">
             <Page pageNumber={1} scale={width > 786 ? 1.7 : 0.6} />
           </Document>
         </Row>
 
-        <Row style={{ justifyContent: "center", position: "relative" }}>
-          <Button
-            variant="primary"
-            href={pdf}
-            target="_blank"
-            style={{ maxWidth: "250px" }}
-          >
-            <AiOutlineDownload />
-            &nbsp;Download CV
-          </Button>
+        <Row className="resume2">
+          <Document file={pdf} className="d-flex justify-content-center">
+            <Page pageNumber={2} scale={width > 786 ? 1.7 : 0.6} />
+          </Document>
         </Row>
       </Container>
     </div>
