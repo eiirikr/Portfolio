2ed778a8f694ef0b07c44d499cee0ef84c69fbb9
javierjericm <javierjericm@gmail.com>
2025-05-03 23:28:02 +0800

    Added more projects

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -5,6 +5,10 @@ import Particle from "../Particle";
 import portfolio from "../../Assets/Projects/portfolio.png";
 import ecommerce from "../../Assets/Projects/ecommerce.png";
 import votesystem from "../../Assets/Projects/votesystem.png";
+import socialbook from "../../Assets/Projects/socialbook.png";
+import linkedln from "../../Assets/Projects/linkedln.png";
+import strawhat from "../../Assets/Projects/strawhat.png";
+import portfolioTemplate1 from "../../Assets/Projects/portfolio-template1.png";
 
 function Projects() {
   return (
@@ -45,9 +49,53 @@ function Projects() {
               imgPath={votesystem}
               isBlog={false}
               title="Votesystem"
-              description="Developed a responsive online voting system using HTML5, CSS3,Bootstrap, AJAX, jQuery, PHP, and MySQL. The system is designed forseamless functionality across all devices and browsers, ensuringaccessibility for all users. Additionally, it includes a registered domainname for professional hosting and easy access."
+              description="Developed a responsive online voting system using HTML5, CSS3,Bootstrap, AJAX, jQuery, PHP, and MySQL. The system is designed forseamless functionality across all devices and browsers, ensuring accessibility for all users. Additionally, it includes a registered domain name for professional hosting and easy access, hosted in Netlify."
               ghLink="https://github.com/eiirikr/votingsystem"
               demoLink="https://ici-votingsystem.online/"
+            />
+          </Col>
+
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={socialbook}
+              isBlog={false}
+              title="Socialbook"
+              description="A fully responsive web application replicating key Facebook features,including user profiles, posts, likes, and comments. Built with HTMLand CSS, it mimics Facebook’s interface, showcasing my front-endskills and expertise in web structure and responsive design."
+              ghLink="https://github.com/eiirikr/Socialbook"
+              demoLink="https://social-javier-projects.vercel.app/"
+            />
+          </Col>
+
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={linkedln}
+              isBlog={false}
+              title="LinkedIn"
+              description="A fully responsive LinkedIn-inspired web application built with HTML and CSS. It features user profiles, post feeds, likes, and comments—closely replicating LinkedIn's layout and functionality. This project highlights my front-end development skills and ability to create professional UI/UX designs."
+              ghLink="https://github.com/eiirikr/LinkedIn"
+              demoLink="https://linkedln-javier-projects.vercel.app/"
+            />
+          </Col>
+
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={strawhat}
+              isBlog={false}
+              title="Strawhat"
+              description="A fully responsive LinkedIn-inspired web application built with HTML and CSS. It features user profiles, post feeds, likes, and comments—closely replicating LinkedIn's layout and functionality. This project highlights my front-end development skills and ability to create professional UI/UX designs."
+              ghLink="https://github.com/eiirikr/static-app-1"
+              demoLink="https://strawhat-javier-projects.vercel.app/"
+            />
+          </Col>
+
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={portfolioTemplate1}
+              isBlog={false}
+              title="PortfolioTemplate1"
+              description="A fully responsive LinkedIn-inspired web application built with HTML and CSS. It features user profiles, post feeds, likes, and comments—closely replicating LinkedIn's layout and functionality. This project highlights my front-end development skills and ability to create professional UI/UX designs."
+              ghLink="https://github.com/eiirikr/static-portfolio1"
+              demoLink="https://portfolio1-javier-projects.vercel.app/"
             />
           </Col>
         </Row>
